4d152ce54cb137fe0b403a9063bbfc9600642f8d
Olaboye David <dtobi59@gmail.com>
2022-02-21 22:38:07 +0100

    initial commit

---
+++ blockchain/hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "kovan",
+  networks: {
+      hardhat: {
+          // // If you want to do some forking, uncomment this
+          // forking: {
+          //   url: MAINNET_RPC_URL
+          // }
+      },
+      localhost: {
+      },
+      kovan: {
+          url: process.env.KOVAN_RPC_URL,
+          accounts: [process.env.PRIVATE_KEY],
+          saveDeployments: true,
+      }
+  },
+  solidity: "0.8.4",
+};
